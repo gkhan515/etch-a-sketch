4381511bf827d8362ae80938e27e584d0fee1950
Gibron Khan <khangibron@gmail.com>
2022-12-21 20:31:23 -0500

    add coloring functionality

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const sketchpad = document.querySelector('.sketchpad');
 
-freshGrid(16);
+freshGrid(100);
 
 function freshGrid (dimension) {
   sketchpad.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
   sketchpad.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
 
   for (let i = 0;i < dimension * dimension;i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
+    let square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseover', colorIn);
     sketchpad.appendChild(square);
   }
+}
+
+function colorIn () {
+  this.style.backgroundColor = 'black';
 }
